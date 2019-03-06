88c2651354cb4a013a0ca05b464d09e6e662c77a
vaibhav.medavarapu@gmail.com <vaibhav.medavarapu@gmail.com>
2019-03-05 18:12:56 -0600

    Static files.

--- StaticFiles/Program.cs
+++ StaticFiles/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace StaticFiles
+namespace CrudEmployee
 {
     public class Program
     {
--- StaticFiles/Startup.cs
+++ StaticFiles/Startup.cs
@@ -5,59 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace StaticFiles
+namespace CrudEmployee
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env )
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+//This is the connection which acts as middlware
+    FileServerOptions defaultfilesoptions = new FileServerOptions();
+    defaultfilesoptions.DefaultFilesOptions.DefaultFileNames.Clear();//Clears the previous connection
+    defaultfilesoptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+app.UseFileServer(defaultfilesoptions);
+            app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            
 
-            app.UseMvc(routes =>
+            app.Run(async (context) =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                await context.Response.WriteAsync("Hello loli");
             });
         }
     }
